bc2edfa296a9d4bc9b2137b3d467a4f4f4bf9b8e
Nguyen Trong Dat <trongdat11ct@gmail.com>
2025-09-18 10:49:41 +0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   // Set base for GitHub Pages project site
-  // https://<user>.github.io/PopMartWeb_PrimeBu/
-  base: '/PopMartWeb_PrimeBu/',
+  // https://<user>.github.io/PopMartWebTaoLink/
+  base: '/PopMartWebTaoLink/',
 })
+
+
 
